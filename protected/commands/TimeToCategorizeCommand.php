42092c98ea7d063954e630aa0a44e451d4ae786f
info@magnusbilling.com <info@magnusbilling.com>
2018-10-29 15:52:26 -0300

    script to set operator to free after time in categorizing

---
+++ protected/commands/TimeToCategorizeCommand.php
@@ -0,0 +1,43 @@
+<?php
+class TimeToCategorizeCommand extends ConsoleCommand
+{
+    public $config;
+
+    public function run($args)
+    {
+        for (;;) {
+
+            $modelOperatorStatus = OperatorStatus::model()->findAll('categorizing = 1');
+
+            foreach ($modelOperatorStatus as $key => $operatorStatus) {
+                $modelCampaign = Campaign::model()->findByPk((int) $operatorStatus->id_campaign);
+                $modeUser      = User::model()->findByPk($operatorStatus->id_user);
+
+                //calcula a media do tempo gasto para categorizar e set o time que categorizou para pegar e calcular o tempo livre ate a proxima chamada
+                if (isset($operatorStatus->time_start_cat) && $operatorStatus->time_start_cat > 0) {
+
+                    echo $time_to_call = time() - $operatorStatus->time_start_cat;
+
+                    if ($time_to_call > $args[0]) {
+
+                        $modeUser->id_current_phonenumber = null;
+                        $modeUser->save();
+
+                        OperatorStatus::model()->updateByPk($operatorStatus->id, array(
+                            'time_start_cat' => 0,
+                            'media_to_cat'   => $args[0],
+                            'time_free'      => time(),
+                            'cant_cat'       => $operatorStatus->cant_cat + 1,
+                            'categorizing'   => 0,
+                        ));
+
+                        OperatorStatusManager::unPause($operatorStatus->id_user, $modelCampaign, 1, $modeUser->username);
+                    }
+                }
+
+            }
+
+            sleep(3);
+        }
+    }
+}
